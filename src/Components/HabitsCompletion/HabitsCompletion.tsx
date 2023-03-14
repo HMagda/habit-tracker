52dda204ae948c5edd2fac49980244755b2b6472
Magdalena <magdalenahyra@wp.pl>
2023-03-14 23:12:11 +0100

    Edited HabitsCompletion component to warn user if he wants to change status of habit on different day than today, and edited styling for upcoming days

--- src/Components/HabitsCompletion/HabitsCompletion.tsx
+++ src/Components/HabitsCompletion/HabitsCompletion.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {HiX, HiCheck} from 'react-icons/hi';
+import {FiCircle} from 'react-icons/fi';
 import './HabitsCompletion.modules.scss';
-import {Habit} from '../../utils';
+import {Habit, daysOfWeek} from '../../utils';
 
 interface HabitsArray {
   habits: Habit[];
@@ -16,7 +17,24 @@ interface CompletedDays {
 const HabitsCompletion: React.FC<HabitsArray> = ({habits}) => {
   const [completedDays, setCompletedDays] = useState<CompletedDays>({});
 
+  useEffect(() => {
+    console.log('completedDays:', completedDays);
+  });
+
+  const today = new Date().toLocaleString('en-GB', {
+    weekday: 'long',
+  });
+  const todayIndex = daysOfWeek.indexOf(today);
+
   const handleMarkCompleted = (id: string, day: string) => {
+    if (new Date().toLocaleString('en-GB', {weekday: 'long'}) !== day) {
+      const confirmed = window.confirm(
+        'The chosen day is not today, are you sure you want to change the habit status?'
+      );
+      if (!confirmed) {
+        return;
+      }
+    }
     setCompletedDays((prev) => {
       const prevHabit = prev[id] || {};
       return {
@@ -25,10 +43,6 @@ const HabitsCompletion: React.FC<HabitsArray> = ({habits}) => {
       };
     });
   };
-
-  useEffect(() => {
-    console.log('completedDays:', completedDays);
-  });
 
   return (
     <div className='habits-wrapper'>
@@ -43,31 +57,34 @@ const HabitsCompletion: React.FC<HabitsArray> = ({habits}) => {
           <div key={index} className='habit'>
             <h3>{habit.habitName}</h3>
             <div className='btns-container'>
-              {habit.days.map((day: string) => (
-                <div className='single-btn-container' key={day}>
-                  <button
-                    key={day}
-                    onClick={() => handleMarkCompleted(habit.habitName, day)}
-                    className={
-                      completedDays[habit.habitName]?.[day]
-                        ? 'completed'
-                        : 'uncompleted'
-                    }
-                    disabled={
-                      new Date().toLocaleString('en-GB', {
-                        weekday: 'long',
-                      }) !== day
-                    }
-                  >
-                    {day}
-                  </button>
-                  {completedDays[habit.habitName]?.[day] ? (
-                    <HiCheck className='check-icon' />
-                  ) : (
-                    <HiX className='x-icon' />
-                  )}
-                </div>
-              ))}
+              {habit.days.map((day: string) => {
+                const dayIndex = daysOfWeek.indexOf(day);
+                const isUpcoming = dayIndex > todayIndex;
+                return (
+                  <div className='single-btn-container' key={day}>
+                    <button
+                      key={day}
+                      onClick={() => handleMarkCompleted(habit.habitName, day)}
+                      className={
+                        completedDays[habit.habitName]?.[day]
+                          ? 'completed'
+                          : isUpcoming
+                          ? 'upcoming'
+                          : 'uncompleted'
+                      }
+                    >
+                      {day}
+                    </button>
+                    {completedDays[habit.habitName]?.[day] ? (
+                      <HiCheck className='check-icon' />
+                    ) : isUpcoming ? (
+                      <FiCircle className='circle-icon' />
+                    ) : (
+                      <HiX className='x-icon' />
+                    )}
+                  </div>
+                );
+              })}
             </div>
           </div>
         )
