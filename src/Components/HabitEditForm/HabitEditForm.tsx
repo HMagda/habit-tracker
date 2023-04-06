3262b63e12fbccf7f5973db070ecc822bb33b03a
Maciej <m.szymeczko@dac.digital>
2023-04-06 14:40:40 +0200

    fix edit form

--- src/Components/HabitEditForm/HabitEditForm.tsx
+++ src/Components/HabitEditForm/HabitEditForm.tsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
-import {baseUrl, daysOfWeek, Habit, HabitDay} from '../../utils';
+import {baseUrl, daysOfWeek, Habit, HabitDay, HabitForToday} from '../../utils';
 import './HabitEditForm.modules.scss';
 
 const HabitEditForm: React.FC<{
   habitsArr: Habit[];
   setHabitsArr: React.Dispatch<React.SetStateAction<Habit[]>>;
+  habitsForTodayArr: HabitForToday[];
+  setHabitsForTodayArr: React.Dispatch<React.SetStateAction<HabitForToday[]>>;
+  todayIndex: number;
   editHabitId: string;
   setEditHabitId: React.Dispatch<React.SetStateAction<string>>;
   editHabitName: string;
@@ -12,6 +15,9 @@ const HabitEditForm: React.FC<{
 }> = ({
   habitsArr,
   setHabitsArr,
+  habitsForTodayArr,
+  setHabitsForTodayArr,
+  todayIndex,
   editHabitId,
   setEditHabitId,
   editHabitName,
@@ -24,11 +30,20 @@ const HabitEditForm: React.FC<{
   const handleDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const day = Number(e.target.id);
     console.log(day);
+
     setDays((days) => {
-      if (days.map((day) => day.dayOfWeek).includes(day)) {
+      const dayIndex = days.findIndex((d) => d.dayOfWeek === day);
+
+      if (dayIndex !== -1) {
         return days.filter((d) => d.dayOfWeek !== day);
       } else {
-        return [...days, {dayOfWeek: day, completed: false}];
+        const newDay: HabitDay = {
+          dayOfWeek: day,
+          dateOfWeek: '',
+          completed: false,
+        };
+
+        return [...days, newDay];
       }
     });
   };
@@ -65,12 +80,35 @@ const HabitEditForm: React.FC<{
           habit.id === editHabitId ? {...habit, habitName, days} : habit
         );
         setHabitsArr(updatedHabitsArr);
+
+        if(!days.map((day) => day.dayOfWeek).includes(todayIndex)
+              && habitsForTodayArr.map((todayHabit: HabitForToday) => todayHabit.id).includes(editHabitId)
+        ){
+          //this means that the habit edit removed today's habit
+            console.log('habit edit removed today habit')
+          setHabitsForTodayArr(habitsForTodayArr.filter((habit: HabitForToday) => habit.id !== editHabitId))
+        } else if(habitsForTodayArr.map((todayHabit: HabitForToday) => todayHabit.id).includes(editHabitId)) {
+             console.log('habit edit updated today habit')
+            const updatedHabitsForTodayArr = habitsForTodayArr.map((habit) =>
+              habit.id === editHabitId ? {...habit, habitName} : habit
+            );
+            setHabitsForTodayArr(updatedHabitsForTodayArr);
+        } else {
+            console.log('habit edit added today habit')
+            setHabitsForTodayArr([...habitsForTodayArr, {
+                id: editHabitId,
+                day: todayIndex,
+                habitName: habitName,
+                completed: false
+            }])
+
+        }
       })
       .catch((error) => {
         console.error('Error adding new habit: ', error);
       });
 
-    setEditHabitId("");
+    setEditHabitId('');
   };
 
   return (
@@ -89,17 +127,17 @@ const HabitEditForm: React.FC<{
       <label htmlFor='frequency'>Frequency:</label>
       <div className='days-label-container'>
         {daysOfWeek.map((day, i) => (
-            <label key={day} htmlFor={i.toString()} className='days-label'>
-              <input
-                  type='checkbox'
-                  id={i.toString()}
-                  value={day}
-                  checked={days.some((d) => d.dayOfWeek === i)}
-                  onChange={handleDayChange}
-              />
-              <div className='days-checkbox'></div>
-              <div className='day'> {day}</div>
-            </label>
+          <label key={day} htmlFor={i.toString()} className='days-label'>
+            <input
+              type='checkbox'
+              id={i.toString()}
+              value={day}
+              checked={days.some((d) => d.dayOfWeek === i)}
+              onChange={handleDayChange}
+            />
+            <div className='days-checkbox'></div>
+            <div className='day'>{day}</div>
+          </label>
         ))}
       </div>
       <button type='submit' disabled={warning}>
