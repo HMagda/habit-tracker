bbcfec54314e5317ff0721b199e25789015655b9
Magdalena <magdalenahyra@wp.pl>
2023-04-07 15:19:19 +0200

    Edited db.json and Heatmap

--- src/Components/Heatmap/Heatmap.tsx
+++ src/Components/Heatmap/Heatmap.tsx
@@ -1,46 +1,73 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import './Heatmap.modules.scss';
 import 'react-calendar-heatmap/dist/styles.css';
 
-function getThreeMonthsAgo() {
-  const currentDate = new Date();
-  const threeMonthsAgo = new Date(currentDate);
-  threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
+function getColor(score: number, maxScore: number): string {
+  const percentage = (score / maxScore) * 100;
 
-  return {
-    currentDate,
-    threeMonthsAgo
-  };
+  return percentage >= 80 ? 'color-scale-5' :
+         percentage >= 60 ? 'color-scale-4' :
+         percentage >= 40 ? 'color-scale-3' :
+         percentage >= 20 ? 'color-scale-2' :
+                            'color-scale-1';
 }
 
+type DayData = {
+  date: string;
+  score: number;
+  maxScore: number;
+};
+
+type HeatmapData = {
+  startDate: string;
+  endDate: string;
+  days: DayData[];
+};
+
 const Heatmap = () => {
-  const { currentDate, threeMonthsAgo } = getThreeMonthsAgo();
+  const [data, setData] = useState<HeatmapData[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/heatmap', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log('data for heatmap:', data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []);
 
   return (
     <div className='heatmap'>
-      <CalendarHeatmap
-          startDate={threeMonthsAgo}
-          endDate={currentDate}
-        values={[
-          {date: '2023-02-21', count: 4},
-          {date: '2023-02-24', count: 5},
-          {date: '2023-03-01', count: 1},
-          {date: '2023-03-03', count: 2},
-          {date: '2023-03-04', count: 3},
-          // ...and so on
-        ]}
-        classForValue={(value) => {
-          if (!value) {
-            return 'color-empty';
-          }
-          return `color-scale-${value.count}`;
-        }}
-        // horizontal={false}
-        gutterSize={5}
-        onClick={(value) => console.log('clicked date', value, value.count)}
-        showWeekdayLabels={false}
-      />
+      {data.length > 0 && (
+        <CalendarHeatmap
+          startDate={new Date(data[0].startDate)}
+          endDate={new Date(data[0].endDate)}
+          values={data[0].days}
+          classForValue={(value) => {
+            if (!value) {
+              return 'color-empty';
+            }
+            return getColor(value.score, value.maxScore);
+          }}
+          // horizontal={false}
+          gutterSize={5}
+          onClick={(value) => console.log('clicked date', value, value.count)}
+          showWeekdayLabels={false}
+        />
+      )}
     </div>
   );
 };
