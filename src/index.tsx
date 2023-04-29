cf850d5c757360668c2f3a6c852dd6fa85da3fe8
Maciej <m.szymeczko@dac.digital>
2023-04-29 03:28:45 +0200

    back to auth0

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,12 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
+
+
+<React.StrictMode>
         <Auth0Provider
-            domain={process.env.REACT_APP_AUTH0_DOMAIN!!}
-            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!!}
+            domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
             authorizationParams={{
                 redirect_uri: window.location.origin,
             }}
